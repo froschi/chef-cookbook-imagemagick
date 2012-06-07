08270357b019faab288954ae172d3f1966374785
Thorsten Fischer <thorsten@froschi.org>
2012-06-07 17:56:48 +0200

    Add dependencies for imagemagick on lucid.
    Don't ask how many there are.
    
    Signed-off-by: Thorsten Fischer <thorsten@froschi.org>

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,4 +1,25 @@
-packages = %w/
+packages = Array.new
+
+case node[:lsb][:codename]
+when "lucid"
+  include_recipe "libbz2"
+  include_recipe "libfreetype"
+  include_recipe "libgomp"
+  include_recipe "libice"
+  include_recipe "libjpeg"
+  include_recipe "liblcms"
+  include_recipe "libltdl"
+  include_recipe "libmagickcore"
+  include_recipe "libmagickwand"
+  include_recipe "libpng"
+  include_recipe "libsm"
+  include_recipe "libtiff"
+  include_recipe "libxext"
+  include_recipe "libxt"
+
+end
+
+packages |= %w/
   imagemagick
 /
 
